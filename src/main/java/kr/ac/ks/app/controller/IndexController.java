23cfd58071b216f26d1339798bdfe268530b10b7
Sangkon Han <sigmadream@gmail.com>
2020-04-12 21:50:40 +0900

    Added 05주차 예제

--- src/main/java/kr/ac/ks/app/controller/IndexController.java
+++ src/main/java/kr/ac/ks/app/controller/IndexController.java
@@ -3,7 +3,10 @@ package kr.ac.ks.app.controller;
 import kr.ac.ks.app.domain.Student;
 import kr.ac.ks.app.repository.StudentRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
@@ -15,10 +18,42 @@ public class IndexController {
     }
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
         Iterable<Student> students = studentRepository.findAll();
-        students.forEach(obj -> System.out.println(obj.getName()));
+        model.addAttribute("students", students);
         return "index";
     }
 
+    // Create
+    @GetMapping("/add")
+    public String showAddForm(Student student) {
+        return "add-student";
+    }
+
+    @PostMapping("/add")
+    public String addStudent(Student student) {
+        studentRepository.save(student);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        model.addAttribute("student", student);
+        return "update-student";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateStudent(Student student) {
+        studentRepository.save(student);
+        return "redirect:/";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteStudent(@PathVariable("id") Long id, Model model) {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        studentRepository.delete(student);
+        model.addAttribute("students", studentRepository.findAll());
+        return "redirect:/";
+    }
 }
