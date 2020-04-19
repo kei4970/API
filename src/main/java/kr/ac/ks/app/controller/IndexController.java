1b8cdd56cc6b834d821a685beee3ec7133ed941f
Sangkon Han <sigmadream@gmail.com>
2020-04-20 00:08:12 +0900

    Added 06주차 예제

--- src/main/java/kr/ac/ks/app/controller/IndexController.java
+++ src/main/java/kr/ac/ks/app/controller/IndexController.java
@@ -1,12 +1,20 @@
 package kr.ac.ks.app.controller;
 
 import kr.ac.ks.app.domain.Student;
+import kr.ac.ks.app.domain.dto.StudentDto;
 import kr.ac.ks.app.repository.StudentRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @Controller
 public class IndexController {
@@ -24,15 +32,20 @@ public class IndexController {
         return "index";
     }
 
-    // Create
     @GetMapping("/add")
-    public String showAddForm(Student student) {
+    public String showAddForm(StudentDto studentDto) {
         return "add-student";
     }
 
     @PostMapping("/add")
-    public String addStudent(Student student) {
+    public String addStudent(@Valid StudentDto studentDto, BindingResult result) {
+        if (result.hasErrors()) {
+            System.out.println("error");
+            return "add-student";
+        }
+        Student student = new Student(studentDto.getName(), studentDto.getEmail(), studentDto.getPhoneNo());
         studentRepository.save(student);
+        System.out.println(student.getName());
         return "redirect:/";
     }
 
--- src/main/java/kr/ac/ks/app/domain/Student.java
+++ src/main/java/kr/ac/ks/app/domain/Student.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Student {
@@ -13,9 +15,7 @@ public class Student {
     private Long id;
 
     private String name;
-
     private String email;
-
     private String phoneNo;
 
     public Student() {
